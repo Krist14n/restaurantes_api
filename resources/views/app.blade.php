72b29aee170ccc92d8991e0af8d70c0502b20eea
kristian <kristian_tapia@yahoo.com>
2015-09-09 09:01:04 -0500

    Agrega Zonas 13

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,7 +20,8 @@
 		<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
-	<script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>	
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/r/bs-3.3.5/dt-1.10.9,b-1.0.3/datatables.min.css"/>
+	<script type="text/javascript" src="https://cdn.datatables.net/r/bs-3.3.5/dt-1.10.9,b-1.0.3/datatables.min.js"></script>
 	<script type="text/javascript">
 	$(document).ready(function(){
 		$('#table_id').DataTable();
