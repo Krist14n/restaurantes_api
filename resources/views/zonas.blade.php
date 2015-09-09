a69103e214e272f89b37bde7d7188fea6c2d3a18
kristian <kristian_tapia@yahoo.com>
2015-09-09 09:03:41 -0500

    Agrega Zonas 14

--- resources/views/zonas.blade.php
+++ resources/views/zonas.blade.php
@@ -21,16 +21,18 @@
 	        <th scope="row">{{ $zona->id }}</th>
 	    	<td>{{ $zona->zona }}</td>
 	        <td>
-
-	          	<a href="/zonas/{{ $zona->id }}/edit"><button type="button" class="btn btn-default">
-	  				<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-				</button></a>
+	        <span style="display:inline">
+	          	<a href="/zonas/{{ $zona->id }}/edit">
+	          		<button type="button" class="btn btn-default">
+	  					<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+					</button>
+				</a>
 
 				{!! Form::open(array('route' => array('zonas.destroy', $zona->id), 'method' => 'delete' )) !!}
 				<button type="submit" class="btn btn-danger">
 	  				<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
 				</button>
-
+			</span>	
 				{!! Form::close() !!}
 			</td>
         </tr>
