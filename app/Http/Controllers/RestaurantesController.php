1b09cc54c44f7d8d8a9946043c686d5254a705a2
kristian <kristian_tapia@yahoo.com>
2015-11-11 09:31:39 -0600

    add nespresso 5

--- app/Http/Controllers/RestaurantesController.php
+++ app/Http/Controllers/RestaurantesController.php
@@ -73,6 +73,7 @@ class RestaurantesController extends Controller {
 		$promocion 			   	=	$request->promocion;
 		$nombre 				=	$request->nombre;
 		$descripcion 			= 	$request->descripcion;
+		$nespresso  			= 	$request->nespresso;
 		$calificacion_comida 	= 	$request->calificacion_comida;
 		$calificacion_ambiente 	= 	$request->calificacion_ambiente;
 		$calificacion_servicio 	= 	$request->calificacion_servicio;
@@ -102,6 +103,7 @@ class RestaurantesController extends Controller {
 			'promocion'				=> 	$promocion,
 			'nombre' 				=>	$nombre,
 			'descripcion'			=> 	$descripcion,
+			'nespresso'				=>	$nespresso,
 			'calificacion_comida'	=> 	$calificacion_comida,
 			'calificacion_ambiente' => 	$calificacion_ambiente,
 			'calificacion_servicio' => 	$calificacion_servicio,
