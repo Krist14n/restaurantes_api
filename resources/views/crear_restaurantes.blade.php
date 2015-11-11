b43de28ae415c9c62658b25d1213c0a25be1d42e
kristian <kristian_tapia@yahoo.com>
2015-11-11 10:09:20 -0600

    editar nespresso 6

--- resources/views/crear_restaurantes.blade.php
+++ resources/views/crear_restaurantes.blade.php
@@ -47,8 +47,8 @@
 
 		<div class="form-group">
 			<label for="nespresso">Nespresso</label>
-			Si{!!Form::radio('nespresso', 'false',true ,['class'=> 'form-control', 'id' => 'nespresso'])!!}
-			No{!!Form::radio('nespresso', 'true' ,['class'=> 'form-control', 'id' => 'nespresso'])!!}
+			{Si!!Form::radio('nespresso', 'false',true ,['id' => 'nespresso'])!!}
+			{No!!Form::radio('nespresso', 'true' ,['id' => 'nespresso'])!!}
 		</div>
 
 		<div class="form-group">
