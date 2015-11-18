8eddc6a9a21b5a487baedfb65938aaa8f8f68ae6
kristian <kristian_tapia@yahoo.com>
2015-11-18 08:27:41 -0600

    app view menu 6

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -25,6 +25,7 @@
 	<script type="text/javascript">
 	$(document).ready(function(){
 		$('#table_id').DataTable();
+		$('.dropdown-toggle').dropdown();
 	});
 	</script>
 </head>
@@ -50,7 +51,6 @@
 						<li><a href="{{ url('/auth/login') }}">Login</a></li>
 						<li><a href="{{ url('/auth/register') }}">Register</a></li>
 					@else
-							
 								<li><a href="{{ url('/zonas') }}">Zonas</a></li>
 								<li><a href="{{ url('/planes') }}">Planes</a></li>
 								<li><a href="{{ url('/cocinas') }}">Tipo Cocina</a></li>
@@ -58,7 +58,7 @@
 						
 								<li><a href="{{ url('/regiones') }}">Regiones del Mundo</a></li>
 								<li><a href="{{ url('/ciudades') }}">Ciudades</a></li>
-								<li><a href="{{ url('/restaurantes_internacionales') }}">Restaurantes</a></li>
+								<li><a href="{{ url('/restaurantes_internacionales') }}">Restaurantes Internacionales</a></li>
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
